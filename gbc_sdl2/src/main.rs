ad1eca61c0d8e4cbf138de48d485c7fd3d766a99
Cory Lanza <corylanza@gmail.com>
2020-06-29 22:26:43 -0400

    Added FPS count to title in SDL2

--- gbc_sdl2/src/main.rs
+++ gbc_sdl2/src/main.rs
@@ -12,7 +12,7 @@ use sdl2::event::Event;
 
 use std::fs::File;
 use std::io::prelude::*;
-//use std::time::Instant;
+use std::time::Instant;
 
 mod display;
 use display::SdlDisplay;
@@ -67,6 +67,9 @@ fn main() -> Result<(), String> {
         }
         
         let mut event_pump = sdl_context.event_pump()?;
+
+        let mut timer = Instant::now();
+        let mut framecount = 0;
 
         'main: loop {
             for event in event_pump.poll_iter() {
@@ -134,13 +137,14 @@ fn main() -> Result<(), String> {
             }
             
             gbc.run_one_frame(&mut display);
+            framecount += 1;
             // To reintroduce fps count use below
-            // let elapsed = self.timer.elapsed().as_millis();
-            // if elapsed > 1000 {
-            //     self.timer = Instant::now();
-            //     println!("fps {}", self.framecount as f32 / (elapsed as f32 / 1000.0));
-            //     self.framecount = 0;
-            // }
+            let elapsed = timer.elapsed().as_millis();
+            if elapsed > 1000 {
+                timer = Instant::now();
+                display.set_title(format!("Rusty GBC - {:.0} fps", framecount as f32 / (elapsed as f32 / 1000.0)));
+                framecount = 0;
+            }
         }
         Ok(())
     } else {
